85b433fc8a96f6f83a890028e8fec05128cbb171
Troubadour Reign <109498287+Troubadour-Reign@users.noreply.github.com>
2025-06-24 22:42:34 +0200

    Initial commit for Next.js app

---
+++ app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+function Hello() {
+    console.log("I am a client component");
+
+    return (
+        <div>
+            <h1 className="text-3xl">Hello Next.js</h1>
+        </div>
+    );
+}
+
+export default Hello;
